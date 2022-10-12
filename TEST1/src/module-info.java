e5dfff1eff53fa1897e4464f4d0b0767552d73a6
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2022-10-12 19:08:16 +0900

    1012

---
+++ TEST1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ITWILL
+ *
+ */
+module TEST1 {
+}
---
+++ TEST1/src/test_/test.java
@@ -0,0 +1,9 @@
+package test_;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+}
